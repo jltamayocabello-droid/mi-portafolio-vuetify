82226446a91cfb9fa9c9571f0741e4dc034dce19
Jorge <jltamayocabello@gmail.com>
2026-03-05 14:38:07 -0300

    Se elimina boton inicio

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -4,7 +4,6 @@
       <v-container class="d-flex align-center">
         <v-app-bar-title class="font-weight-bold">JT.DEV</v-app-bar-title>
         <v-spacer />
-        <v-btn to="/" variant="text">Inicio</v-btn>
         <v-btn href="#proyectos" variant="text">Proyectos</v-btn>
         <v-btn href="https://linkedin.com/in/tu-perfil" icon="mdi-linkedin" target="_blank" variant="text" />
         <v-btn href="https://github.com/jltamayocabello-droid" icon="mdi-github" target="_blank" variant="text" />
