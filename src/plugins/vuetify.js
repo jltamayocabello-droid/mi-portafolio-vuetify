42215573e64252123ad8d2a74ce471830f836d9c
Jorge <jltamayocabello@gmail.com>
2026-03-05 12:50:04 -0300

    se incorpora diseño dark premium

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,19 +1,19 @@
-/**
- * plugins/vuetify.ts
- *
- * Framework documentation: https://vuetifyjs.com`
- */
-
-// Composables
-import { createVuetify } from 'vuetify'
-// Styles
 import '@mdi/font/css/materialdesignicons.css'
-
 import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
 
-// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'system',
+    defaultTheme: 'dark', // 🌙 Esto activa el modo oscuro por defecto
+    themes: {
+      dark: {
+        colors: {
+          background: '#121212', // Negro profundo estilo Craftivo
+          surface: '#1E1E1E', // Gris oscuro para tarjetas
+          primary: '#4FC08D', // Verde Vue
+          secondary: '#ffe162', // Amarillo Pinia
+        },
+      },
+    },
   },
 })
