1056cdf22bfce6131b57d20edd75ff93165c197b
Jorge <jltamayocabello@gmail.com>
2026-03-08 21:12:35 -0300

    Se agrega comportamiento responsivo a el footer

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,45 +1,90 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const drawer = ref(false)
+</script>
+
 <template>
   <v-app>
     <v-app-bar border color="background" flat>
       <v-container class="d-flex align-center">
         <v-app-bar-title class="font-weight-bold">JT.DEV</v-app-bar-title>
         <v-spacer />
-        <v-btn href="#formacion" variant="text">
-          <v-icon icon="mdi-school" start />
-          Trayectoria
-        </v-btn>
-        <v-btn href="#proyectos" variant="text">
-          <v-icon icon="mdi-briefcase" start />
-          Proyectos Destacados
-        </v-btn>
-        <v-btn
-          href="https://www.linkedin.com/in/jorge-tamayo-c/"
-          icon="mdi-linkedin"
-          target="_blank"
-          variant="text"
-        />
-        <v-btn
-          href="https://github.com/jltamayocabello-droid"
-          icon="mdi-github"
-          target="_blank"
-          variant="text"
-        />
-        <v-btn
-          href="mailto:jltamayocabello@gmail.com"
-          icon="mdi-email-outline"
-          variant="text"
-        />
+        <div class="d-none d-md-flex align-center">
+          <v-btn href="#formacion" variant="text">
+            <v-icon icon="mdi-school" start />
+            Trayectoria
+          </v-btn>
+          <v-btn href="#proyectos" variant="text">
+            <v-icon icon="mdi-briefcase" start />
+            Proyectos Destacados
+          </v-btn>
+          <v-btn
+            href="https://www.linkedin.com/in/jorge-tamayo-c/"
+            icon="mdi-linkedin"
+            target="_blank"
+            variant="text"
+          />
+          <v-btn
+            href="https://github.com/jltamayocabello-droid"
+            icon="mdi-github"
+            target="_blank"
+            variant="text"
+          />
+          <v-btn
+            href="mailto:jltamayocabello@gmail.com"
+            icon="mdi-email-outline"
+            variant="text"
+          />
+        </div>
+        <v-app-bar-nav-icon class="d-md-none" @click="drawer = !drawer" />
       </v-container>
     </v-app-bar>
 
+    <v-navigation-drawer v-model="drawer" location="right" temporary>
+      <v-list>
+        <v-list-item
+          href="#formacion"
+          prepend-icon="mdi-school"
+          title="Trayectoria"
+          @click="drawer = false"
+        />
+        <v-list-item
+          href="#proyectos"
+          prepend-icon="mdi-briefcase"
+          title="Proyectos Destacados"
+          @click="drawer = false"
+        />
+        <v-divider />
+        <v-list-item
+          href="https://www.linkedin.com/in/jorge-tamayo-c/"
+          prepend-icon="mdi-linkedin"
+          target="_blank"
+          title="LinkedIn"
+        />
+        <v-list-item
+          href="https://github.com/jltamayocabello-droid"
+          prepend-icon="mdi-github"
+          target="_blank"
+          title="GitHub"
+        />
+        <v-list-item
+          href="mailto:jltamayocabello@gmail.com"
+          prepend-icon="mdi-email-outline"
+          title="Contacto"
+        />
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
       <router-view />
     </v-main>
 
     <v-footer class="d-flex flex-column pa-8" color="background">
       <!-- Contacto-->
-      <div class="d-flex justify-center ga-10 align-center w-100 text-caption text-medium-emphasis">
-        <div class="d-flex flex-column align-start ">
+      <div
+        class="d-flex flex-wrap justify-center ga-5 ga-md-10 align-center w-100 text-caption text-medium-emphasis"
+      >
+        <div class="d-flex flex-column align-start">
           <strong>Contacto: </strong>
           <a href="mailto:jltamayocabello@gmail.com" style="color: inherit">
             <v-icon icon="mdi-email-outline" size="13" />
@@ -50,7 +95,7 @@
             +56 9 81986512
           </a>
         </div>
-        <v-divider vertical />
+        <v-divider class="d-none d-md-flex" vertical />
         <!-- Redes Sociales -->
         <div class="d-flex gap-2">
           <v-btn
